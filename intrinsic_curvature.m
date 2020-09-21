5da3e356cc9d76e5946ee632af68040841602afb
Benjamin Walker <benjaminwalker@blueyonder.co.uk>
2020-09-21 10:24:55 +0100

    Intrinsic curvature example updated to match that of the accompanying manuscript.

--- intrinsic_curvature.m
+++ intrinsic_curvature.m
@@ -6,8 +6,8 @@ function curv = intrinsic_curvature(t,N)
 	s = (0:1/N:1)'; s = s(1:end-1);
 	% Specify the intrinsic curvatures in the d1,d2,d3 directions.
 	d1_comp = 0 * s;
-	d2_comp = 0 * s + 3.3;
-	d3_comp = 0 * s + 5.5;
+	d2_comp = 0 * s + pi;
+	d3_comp = 0 * s + 2*pi;
 	% Package the output.
 	curv = [d1_comp, d2_comp, d3_comp];
 	% Remove this line to include the example intrinsic curvature.
