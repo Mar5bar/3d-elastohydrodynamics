bd73b58f620d0bd74340cb185bd8c56b6ab096e0
Benjamin Walker <benjaminwalker@blueyonder.co.uk>
2020-11-04 14:15:02 +0000

    odeabort updated to test using inequalities, which appears to be more robust.

--- odeabort.m
+++ odeabort.m
@@ -12,10 +12,10 @@ isterminal(1) = 1;
 direction(1) = 0;
 
 % We test if our values of THETA are too close to 0 or pi.
-value(2)=min(pi-Z(4:N+3)) - delta;
+value(2)=min(pi-Z(4:N+3)) < delta;
 isterminal(2) = 1;
 direction(2) = 0;
 
-value(3)=min(Z(4:N+3)) - delta;
+value(3)=min(Z(4:N+3)) < delta;
 isterminal(3) = 1;
 direction(3) = 0;
